ad1d8c0190cb70d34696049b1c1a085e9d7c59ca
Naoya Sakabe <k.exa.apostrophe@gmail.com>
2017-04-18 18:55:36 +0900

    implement session control

--- main.go
+++ main.go
@@ -11,13 +11,26 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"sync"
+	"time"
 )
+
+const lilypondBinPath = "/Users/nao/bin/lilypond"
+const listenPort = "8080"
+const hourToExpireSession = 5
+
+type Session struct {
+	sessionID  uuid.UUID
+	issuedTime time.Time
+}
+
+var sessions []Session
 
 func main() {
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/post-score", scoreHandler)
 	http.HandleFunc("/get-score/", getScoreHandler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+listenPort, nil)
 }
 
 type Initial struct {
@@ -26,9 +39,45 @@ type Initial struct {
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, _ := template.ParseFiles("index.template.html")
-	var uuidstr string = uuid.NewV4().String()
-	initial := Initial{SessionID: uuidstr}
+	var newuuid uuid.UUID = uuid.NewV4()
+	var newuuidstr string = newuuid.String()
+	var newtime time.Time = time.Now()
+	thisSession := Session{newuuid, newtime}
+	sessions = append(sessions, thisSession)
+	initial := Initial{SessionID: newuuidstr}
 	tmpl.Execute(w, initial)
+}
+
+func delete_s(s []Session, i int) []Session {
+	s = append(s[:i], s[i+1:]...)
+	n := make([]Session, len(s))
+	copy(n, s)
+	return n
+}
+
+var sessionsMutex sync.Mutex
+
+func deleteOldSessions() {
+	sessionsMutex.Lock()
+	defer sessionsMutex.Unlock()
+	for i := 0; i < len(sessions); {
+		if sessions[i].issuedTime.Before(time.Now().Add(-time.Duration(hourToExpireSession) * time.Hour)) {
+			delete_s(sessions, i)
+		} else {
+			i += 1
+		}
+	}
+}
+func checkValidSession(sessionID uuid.UUID) bool {
+	deleteOldSessions()
+	sessionsMutex.Lock()
+	defer sessionsMutex.Unlock()
+	for i := range sessions {
+		if uuid.Equal(sessions[i].sessionID, sessionID) {
+			return true
+		}
+	}
+	return false
 }
 
 type PostedScore struct {
@@ -36,72 +85,93 @@ type PostedScore struct {
 	Score     string
 }
 type Output struct {
-	Out string
+	Success bool
+	Message string
 }
 
 var uuid_regex = regexp.MustCompile(`^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[0-5][a-fA-F0-9]{3}-[089aAbB][a-fA-F0-9]{3}-[a-fA-F0-9]{12}$`)
 
 func scoreHandler(w http.ResponseWriter, r *http.Request) {
-	output := Output{"default value"}
+	output := Output{false, "default value(not shown)"}
 	defer func() {
-		fmt.Fprintf(w, output.Out)
+		json_string, _ := json.Marshal(output)
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, string(json_string))
 	}()
 	if r.Method != "POST" {
-		output.Out = "illegal method"
+		output.Message = "illegal method"
 		w.WriteHeader(400)
 		return
 	}
 	if r.Header.Get("Content-Type") != "application/json" {
-		output.Out = "illegal content type"
+		output.Message = "illegal content type"
 		w.WriteHeader(400)
 		return
 	}
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		output.Out = err.Error()
+		output.Message = err.Error()
 		fmt.Println(err.Error())
 		return
 	}
 	postedScore := PostedScore{}
 	err = json.Unmarshal(requestBody, &postedScore)
 	if err != nil {
-		output.Out = err.Error()
+		output.Message = err.Error()
 		fmt.Println(err.Error())
 		return
 	}
 
-	const binPath = "/Users/nao/bin/lilypond"
-	var id string = postedScore.SessionID
-	var valid_id bool = uuid_regex.MatchString(id)
-	if !valid_id {
-		output.Out = "invalid SessionID"
+	var id_str string = postedScore.SessionID
+	id, err := uuid.FromString(id_str)
+	if err != nil {
+		output.Message = "invalid SessionID"
 		return
 	}
-	cmd := exec.Command(binPath, "-o", "output/"+id, "/dev/stdin")
+	var valid_session bool = checkValidSession(id)
+	if !valid_session {
+		output.Message = "no such session"
+		return
+	}
+
+	cmd := exec.Command(lilypondBinPath, "-o", "output/"+id_str, "/dev/stdin")
 	stdin, err := cmd.StdinPipe()
 	if err != nil {
-		output.Out = err.Error()
+		output.Message = err.Error()
 		return
 	}
-	fmt.Print("Processing: uuid " + string(requestBody))
+	fmt.Println("Processing: uuid " + string(requestBody))
 	go func() {
 		defer stdin.Close()
 		io.WriteString(stdin, postedScore.Score)
 	}()
-	combinedOutput, _ := cmd.CombinedOutput()
-	output.Out = string(combinedOutput)
+	combinedOutput, commandError := cmd.CombinedOutput()
+	if commandError == nil { //exit status
+		output.Success = true
+	} else {
+		output.Success = false
+	}
+	output.Message = string(combinedOutput)
+	//respond to client by deferred function call
 }
 
 func getScoreHandler(w http.ResponseWriter, r *http.Request) {
-	var id string = r.URL.Path[len("/get-score/"):]
-	var valid_id bool = uuid_regex.MatchString(id)
-
-	if !valid_id {
+	var id_str string = r.URL.Path[len("/get-score/"):]
+	id, err := uuid.FromString(id_str)
+	//var valid_id bool = uuid_regex.MatchString(id_str)
+	if err != nil {
 		w.WriteHeader(403)
 		fmt.Fprintf(w, "invalid id!")
 		return
 	}
-	file, err := os.Open("output/" + id + ".pdf")
+	if !checkValidSession(id) {
+		w.WriteHeader(403)
+		fmt.Fprintf(w, "no such session")
+		return
+	}
+
+	filename := "output/" + id_str + ".pdf"
+	file, err := os.Open(filename)
 	if err != nil {
 		w.WriteHeader(404)
 		fmt.Fprintf(w, err.Error())
@@ -109,4 +179,13 @@ func getScoreHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/pdf")
 	_, err = io.Copy(w, file)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+	err = os.Remove(filename)
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
 }
